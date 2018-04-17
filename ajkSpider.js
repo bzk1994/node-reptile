4937583f3229560b7211137e918f240e7bcb065c
bzk1994 <baizk2017@126.com>
2018-04-17 22:39:22 +0800

    success

--- ajkSpider.js
+++ ajkSpider.js
@@ -33,10 +33,59 @@ function startRequest(x){
             console.log(news_item)
             var news_title = $('div.article-title a').text().trim();
 
+            //TODO 存储图片和内容
+            saveContent($, news_title)
+            saveImg($, news_title)
+
+            var nextLink="http://www.ss.pku.edu.cn" + $("li.next a").attr('href');
+            str1 = nextLink.split('-');  //去除掉url后面的中文
+            str = encodeURI(str1[0]);
+
+            if(i<10){
+                //避免访问过快被封IP
+                setTimeout(function(){
+                    fetchPage(str)
+                },5000)
+            }
         });
     }).on('error',function(err){
        console.log(err)
     });
+}
+
+function saveContent($, news_title){
+    $('.article-content p').each(function(index, item){
+        var x = $(this).text()
+        var y = x.substring(0, 2).trim()
+        if(y==''){
+            // 将段落一段段添加到文件名为新闻标题的文件下
+            x += '\n' //换行
+            fs.appendFile('./data/'+news_title+ '.txt',x,'utf-8',function(err){
+                if(err){
+                    console.log(err)
+                }
+            })
+        }
+    })
+}
+
+function saveImg($, news_title){
+    $('.article-content img').each(function(index, item){
+        var img_title = $(this).parent().next().text().trim()
+        if(img_title.length>35 || img_title==''){
+            img_title = 'Null'
+        }
+        var file_name = img_title + '.jpg'
+        var src = 'http://www.ss.pku.edu.cn'+$(this).attr('src')
+
+        //使用request 模块获取图片资源
+        request.head(src, function(err,res,body){
+            if(err){
+                console.log(err)
+            }
+        })
+        request(src).pipe(fs.createWriteStream('./img/'+news_title+'---'+file_name))
+    })
 }
 
 fetchPage(url)
